99676df61d382f1c79f65b9d82988918bd1d4d18
SathvikGangaraju <156886619+SathvikGangaraju@users.noreply.github.com>
2025-06-26 02:22:02 +0530

    pushing

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
+      <h1 className="text-3xl font-bold text-gray-800 text-center">
+        🚀 Startup Evaluator is Live!
+      </h1>
+    </div>
+  );
+}
